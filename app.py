119951edcf3ccbfdb9edec94b6f3fe1e8efcee44
fatema123elfiky <fatmaelfeky922@gmail.com>
2025-11-29 06:01:07 +0200

    Adding some css

--- app.py
+++ app.py
@@ -5,7 +5,91 @@ import VectorQuantization as comp
 import os
 
 st.set_page_config(page_title="Image Compressor", layout="centered")
-st.title("Image Compression")
+st.title(" * Compressor * ")
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-color: #89CFF0 ;
+    }
+   
+    .main {
+        background-color:#89CFF0;
+    }
+    .body{
+    background-color:#89CFF0;
+    }
+
+    
+    h1 {
+        color: #000000 !important;
+        text-align: center;
+        font-weight: 800;
+    }
+
+    
+    .stButton>button {
+        background-color: #3f72af;
+        color: white;
+        border-radius: 8px;
+        padding: 10px 18px;
+        font-size: 16px;
+        transition: 0.3s;
+    }
+
+    
+    .stButton>button:hover {
+        background-color: #112d4e;
+        transform: scale(1.05);
+    }
+
+    
+    .stFileUploader {
+        border: 2px dashed #3f72af;
+        border-radius: 10px;
+        padding: 15px;
+        background-color: #ffffffdd;
+    }
+
+    .stFileUploader:hover {
+        border-color: #112d4e;
+    }
+
+    
+    .stNumberInput>div>div>input {
+        border-radius: 6px;
+        border: 1px solid #3f72af;
+    }
+
+    
+    .stSuccess {
+        background-color: #d4edda !important;
+        border-left: 5px solid #28a745 !important;
+    }
+
+    
+    .centered {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .block-container {
+    max-width: 60% !important;
+    padding: 10rem 8rem !important;
+    
+    border-radius: 12px;
+    
+    box-shadow: 0px 4px 12px rgba(0,0,0,0.1);
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+
+
+st.subheader("Upload & Compress")
 
 file = st.file_uploader("Upload image", type=["png","jpg","bmp"])
 
@@ -18,14 +102,7 @@ bw = st.number_input("Block Width", min_value=1, value=4, step=1)
 cs = st.number_input("Codebook Size", min_value=2, value=16, step=1)
 
 compressbtn = st.button("Compress")
-
-cmp_img = st.file_uploader("Upload compressed image", type=["npy"])
-codebook = st.file_uploader("Upload codebook ", type=["npy"])
-meta_data = st.file_uploader("Upload meta data ", type=["txt"])
-decompressbtn = st.button("Decompress")
-
 if compressbtn and file:
-
     ratio = comp.compress(file, bh, bw, cs)
     st.success(f"Compressed Successfully !! \n")
     st.success(
@@ -35,14 +112,19 @@ if compressbtn and file:
         f"Compression Ratio : {ratio[2]}"
     )
 
+
+
+st.subheader("Decompress")
+
+cmp_img = st.file_uploader("Upload compressed image", type=["npy"])
+codebook = st.file_uploader("Upload codebook ", type=["npy"])
+meta_data = st.file_uploader("Upload meta data ", type=["txt"])
+decompressbtn = st.button("Decompress")
+
+
 if decompressbtn and codebook and cmp_img and meta_data:
 
     comp.decompress(codebook, cmp_img, meta_data)
     st.image(Image.open("C:\\Users\Fatma\PycharmProjects\DataCompression\VecCmp\images\decompressed_image.png"), caption="Decompressed", width=200)
     st.success("Decompression Finished !!")
 
-    #if os.path.exists("decompressed.png"):
-     #   comp.decompress()
-
-    #else:
-        #st.warning("No compressed image found. Please compress first")
